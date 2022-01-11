d772d088442d2ad96467a6ecf7ab60dd581c3075
Jacob Moss <cob.mossy@gmail.com>
2022-01-11 17:11:57 +0000

    dataset split, save and load

--- torte/trainer.py
+++ torte/trainer.py
@@ -1,4 +1,3 @@
-import time
 import torch
 import numpy as np
 
@@ -6,6 +5,7 @@ from collections.abc import Iterable
 from abc import abstractmethod
 from typing import List
 from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import Subset
 from torch.nn import Module
 from time import time
 from pathlib import Path
@@ -20,6 +20,9 @@ class Trainer:
     model: The Model.
     optimizers: list of `torch.optim.Optimizer`s. For when natural gradients are used for variational models.
     dataset: Dataset where t_observed (D, T), m_observed (J, T).
+    batch_size:
+    valid_size: int or float for number of training points or proportion of training points respectively
+    test_size: int or float for number of or proportion of training points respectively
     track_parameters: the keys into `named_parameters()` of parameters that the trainer should track. The
                       tracked parameters can be accessed from `parameter_trace`
     train_mask: boolean mask
@@ -29,6 +32,8 @@ class Trainer:
                  optimizers: List[torch.optim.Optimizer],
                  dataset,
                  batch_size=1,
+                 valid_size=0.,
+                 test_size=0.,
                  device_id=None,
                  track_parameters=None,
                  train_mask=None,
@@ -38,7 +43,26 @@ class Trainer:
         self.optimizers = optimizers
         self.use_natural_gradient = len(self.optimizers) > 1
         self.batch_size = batch_size
-        self.data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+        # Dataset splits
+        dataset_size = len(dataset)
+        if isinstance(valid_size, int):
+            valid_size /= dataset_size
+        if isinstance(test_size, int):
+            test_size /= test_size
+        indices = list(range(dataset_size))
+        np.random.shuffle(indices)
+        valid_split = int(np.floor(valid_size * dataset_size))
+        test_split = int(np.floor(test_size * dataset_size))
+        self.valid_indices, self.test_indices = indices[:valid_split], indices[valid_split:test_split + valid_split]
+        self.train_indices = indices[test_split + valid_split:]
+        valid_data = Subset(dataset, self.valid_indices)
+        test_data = Subset(dataset, self.test_indices)
+        train_data = Subset(dataset, self.train_indices)
+        self.test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+        self.valid_loader = DataLoader(valid_data, batch_size=batch_size, shuffle=False)
+        self.data_loader = DataLoader(train_data, batch_size=batch_size, shuffle=False)
+
         self.losses = None
         self.train_mask = train_mask
         self.checkpoint_dir = checkpoint_dir
@@ -64,14 +88,13 @@ class Trainer:
 
         for epoch in range(epochs):
             epoch_loss = self.single_epoch(epoch=self.num_epochs, **kwargs)
-            t = time.time()
+            t = time()
             times.append((t, epoch_loss))
 
             if isinstance(epoch_loss, Iterable):
                 epoch_loss, loss_breakdown = epoch_loss
             else:
                 loss_breakdown = [epoch_loss]
-
 
             if (epoch % report_interval) == 0:
                 if reporter_callback is not None:
@@ -113,16 +136,22 @@ class Trainer:
                     self.parameter_trace[key].append(params[key].detach().clone())
 
     def save(self, file_prefix='model', save_model=True, save_losses=True, save_optimizer=False):
-        t = int(time())
+        timestamp = int(time())
+        path = Path(f'./{file_prefix}-{timestamp}/')
+        if path.exists():
+            raise IOError('Path seems to already exist.')
+        path.mkdir()
+
+        torch.save((self.train_indices, self.test_indices, self.valid_indices), path / 'indices.pt')
         if save_model:
-            torch.save(self.model.state_dict(), f'{file_prefix}-model-{t}.pt')
+            torch.save(self.model.state_dict(), path / 'model.pt')
 
         if save_losses:
-            torch.save(self.losses, f'{file_prefix}-losses-{t}.pt')
+            torch.save(self.losses, path / 'losses.pt')
 
         if save_optimizer:
             for i, optimizer in enumerate(self.optimizers):
-                torch.save(optimizer.state_dict(), f'{file_prefix}-optim[{i}]-{t}.pt')
+                torch.save(optimizer.state_dict(), path / 'optim[{i}].pt')
 
     def load(self, file_prefix='model', timestamp=''):
         """
@@ -132,12 +161,13 @@ class Trainer:
         :param timestamp:
         :return: list(model, losses, optimizer), each only present if file is found
         """
-        modules = []
-        objects = ['model', 'losses', *[f'optim[{i}]' for i in range(len(self.optimizers))]]
+        path = Path(f'./{file_prefix}-{timestamp}/')
+        modules = dict()
+        objects = ['model', 'indices', 'losses', *[f'optim[{i}]' for i in range(len(self.optimizers))]]
         for object in objects:
-            obj_path = Path(f'{file_prefix}-{object}-{timestamp}.pt')
+            obj_path = Path(path / f'{object}.pt')
             print(obj_path, obj_path.exists())
             if obj_path.exists():
-                modules.append(torch.load(obj_path))
+                modules[object] = torch.load(obj_path)
 
         return modules
